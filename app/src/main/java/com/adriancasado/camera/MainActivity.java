8661ac0a8cc9d90354fecd2ca17492750e09f38a
AdrianCasadoAguilera <152631290+AdrianCasadoAguilera@users.noreply.github.com>
2025-03-11 18:09:16 +0100

    finished

---
+++ app/src/main/java/com/adriancasado/camera/MainActivity.java
@@ -0,0 +1,137 @@
+package com.adriancasado.camera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int RC_PHOTO_PICKER = 0;
+    ActivityResultLauncher<Intent> galleryResultLauncher;
+    ActivityResultLauncher<Intent> cameraResultLauncher;
+
+    Uri photoUri;
+
+    String currentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        galleryResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK) {
+
+                            ImageView imageView = findViewById(R.id.img);
+
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            imageView.setImageURI(uri);
+
+                        }
+                    }
+                }
+        );
+        cameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
+                            ImageView imageView = findViewById(R.id.img);
+                            if (photoUri != null) {
+                                imageView.setImageURI(photoUri);
+                            } else {
+                                Log.e("ERROR", "photoUri is null");
+                            }
+                        }
+                    }
+                }
+        );
+
+
+        Button galleryButton = findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                galleryResultLauncher.launch(intent);
+            }
+        });
+
+        Button cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile;
+                    try {
+                        photoFile = createImageFile();
+                    }catch (IOException e) {
+                        photoFile = null;
+                    }
+
+                    if(photoFile != null) {
+                        photoUri = FileProvider.getUriForFile(MainActivity.this, "com.adriancasado.camera", photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                        cameraResultLauncher.launch(intent);
+                    }
+                }
+            }
+        });
+
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+}
